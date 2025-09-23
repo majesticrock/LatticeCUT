633fb55306acc24d9db558c04be0df378138f951
majesticrock <joshua.althueser@googlemail.com>
2025-09-23 10:33:18 +0200

    MKL Works

--- cmake/MKLFlags.cmake
+++ cmake/MKLFlags.cmake
@@ -4,9 +4,12 @@ function(SET_MKL_FLAGS TARGET)
     if (USE_MKL)
         message(STATUS "Configuring target ${TARGET} to use Intel MKL")
 
-        target_compile_options(${TARGET} PUBLIC $<TARGET_PROPERTY:MKL::MKL,INTERFACE_COMPILE_OPTIONS>)
-        target_include_directories(${TARGET} PUBLIC $<TARGET_PROPERTY:MKL::MKL,INTERFACE_INCLUDE_DIRECTORIES>)
-        target_link_libraries(${TARGET} PUBLIC $<LINK_ONLY:MKL::MKL>)
+        target_compile_options(${TARGET} PRIVATE $<TARGET_PROPERTY:MKL::MKL,INTERFACE_COMPILE_OPTIONS>)
+        target_include_directories(${TARGET} PRIVATE $<TARGET_PROPERTY:MKL::MKL,INTERFACE_INCLUDE_DIRECTORIES>)
+        target_link_libraries(${TARGET} PRIVATE $<LINK_ONLY:MKL::MKL>)
+
+	# Let Eigen use MKL
+	target_compile_definitions(${TARGET} PRIVATE EIGEN_USE_MKL_ALL)
     else()
         message(STATUS "MKL not found or not enabled; ${TARGET} will not use Intel MKL")
     endif()
