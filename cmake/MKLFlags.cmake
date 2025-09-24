f4353a665edc1d7333e90cdff527dfbcc4629f3f
majesticrock <joshua.althueser@googlemail.com>
2025-09-24 15:50:42 +0200

    uni

--- cmake/MKLFlags.cmake
+++ cmake/MKLFlags.cmake
@@ -8,8 +8,8 @@ function(SET_MKL_FLAGS TARGET)
         target_include_directories(${TARGET} PRIVATE $<TARGET_PROPERTY:MKL::MKL,INTERFACE_INCLUDE_DIRECTORIES>)
         target_link_libraries(${TARGET} PRIVATE $<LINK_ONLY:MKL::MKL>)
 
-	# Let Eigen use MKL
-	target_compile_definitions(${TARGET} PRIVATE EIGEN_USE_MKL_ALL MROCK_IEOM_DO_NOT_PARALLELIZE)
+	    # Let Eigen use MKL
+	    target_compile_definitions(${TARGET} PRIVATE EIGEN_USE_MKL_ALL MROCK_IEOM_DO_NOT_PARALLELIZE)
     else()
         message(STATUS "MKL not found or not enabled; ${TARGET} will not use Intel MKL")
     endif()
