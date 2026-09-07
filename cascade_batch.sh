c8e9f96e5184e3044214efbd509d984f1f842e3f
majesticrock <joshua.althueser@googlemail.com>
2026-09-07 14:44:46 +0200

    update slurm script

--- cascade_batch.sh
+++ cascade_batch.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Set architecture: "icelake" or "cascadelake"
-arch="cascadelake"
 LATTICE_TYPE="sc"
-#EXTRA="--exclude=node60"
+EXTRA="--exclude=node60"
 
 input_file="params/for_auto.txt"
 readarray -t NEW_VALUES < "${input_file}"
@@ -61,10 +59,11 @@ for NEW_VALUE in "${NEW_VALUES[@]}"; do
   slurm_path="auto_generated_${CURRENT_TIME}/$NEW_NAME.slurm"
   sed -e "s|#SBATCH --job-name=${LATTICE_TYPE}_cut|#SBATCH --job-name=${LATTICE_TYPE}_${NEW_NAME}_${CURRENT_TIME}|" \
       -e "s|#SBATCH --output=/home/althueser/phd/cpp/LatticeCUT/output_${LATTICE_TYPE}.txt|#SBATCH --output=/home/althueser/phd/cpp/LatticeCUT/output_${CURRENT_TIME}_$NEW_NAME.txt|" \
-      -e "s|^#SBATCH --constraint=.*|#SBATCH --constraint=${arch}|" \
-      -e "s|./build/.*/latticecut .*|./build/${arch}/latticecut auto_generated_${CURRENT_TIME}/$NEW_NAME.config|" \
+      -e "s|^#SBATCH --constraint=.*|#SBATCH --constraint=CascadeLake|" \
+      -e "s|./build/.*/latticecut .*|./build/cascadelake/latticecut auto_generated_${CURRENT_TIME}/$NEW_NAME.config|" \
       slurm/${LATTICE_TYPE}_cascade.slurm > "$slurm_path"
 
   # Submit the job
-  sbatch "$EXTRA" "$slurm_path"
+  #sbatch "$EXTRA" "$slurm_path"
+  sbatch "$slurm_path"
 done
